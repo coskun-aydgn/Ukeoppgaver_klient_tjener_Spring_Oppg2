fc7b0b3d9e35d0e91a6cacf789234e5d133cf0a3
47462 <coskun.aydn@gmail.com>
2024-04-07 12:09:43 +0200

    new

---
+++ src/main/java/com/example/ukeoppgaver_klient_tjener_spring_oppg2/UkeoppgaverKlientTjenerSpringOppg2Application.java
@@ -0,0 +1,13 @@
+package com.example.ukeoppgaver_klient_tjener_spring_oppg2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UkeoppgaverKlientTjenerSpringOppg2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UkeoppgaverKlientTjenerSpringOppg2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ukeoppgaver_klient_tjener_spring_oppg2/UkeoppgaverKlientTjenerSpringOppg2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ukeoppgaver_klient_tjener_spring_oppg2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UkeoppgaverKlientTjenerSpringOppg2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
